1b88c96bef3b4542c1f778d1853f8302b68066d0
z3pro <121992268+z3pro@users.noreply.github.com>
2023-01-06 18:46:58 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+const tabs = $$('.tab-item');
+const panes = $$('.tab-pane');
+const tabActive = $('.tab-item.active');
+const line = $('.tabs .line');
+line.style.left = tabActive.offsetLeft + 'px';
+line.style.width = tabActive.offsetWidth + 'px';
+tabs.forEach((curr,index) => {
+    const pane =panes[index];
+    curr.onclick = function () {
+        $('.tab-item.active').classList.remove('active');
+        $('.tab-pane.active').classList.remove('active');
+        line.style.left = this.offsetLeft + 'px';
+        line.style.width = this.offsetWidth + 'px';
+        this.classList.add('active'); 
+        pane.classList.add('active')
+    }
+})
